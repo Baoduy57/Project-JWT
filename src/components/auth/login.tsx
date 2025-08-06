b52fa488774ccff6b4a928a52a306dbbb2a6a272
BaoDuy <thaibaoduy.0507@gmail.com>
2025-08-06 18:33:43 +0700

    Forgot Password

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -1,110 +1,122 @@
-"use client";
-import { Button, Col, Divider, Form, Input, notification, Row } from "antd";
-import { ArrowLeftOutlined } from "@ant-design/icons";
-import Link from "next/link";
-import { authenticate } from "@/utils/actions";
-import { useRouter } from "next/navigation";
-import ModalReactive from "./modal.reactive";
-import { useState } from "react";
+'use client'
+import { Button, Col, Divider, Form, Input, notification, Row } from 'antd';
+import { ArrowLeftOutlined } from '@ant-design/icons';
+import Link from 'next/link';
+import { authenticate } from '@/utils/actions';
+import { useRouter } from 'next/navigation';
+import ModalReactive from './modal.reactive';
+import { useState } from 'react';
+import ModalChangePassword from './modal.change.password';
 
 const Login = () => {
-  const router = useRouter();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [userEmail, setUserEmail] = useState("");
+    const router = useRouter();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [userEmail, setUserEmail] = useState("");
 
-  const onFinish = async (values: any) => {
-    const { username, password } = values;
-    setUserEmail("");
+    const [changePassword, setChangePassword] = useState(false);
 
-    // trigger Sign-in
-    const res = await authenticate(username, password);
-    if (res?.error) {
-      // error
-      if (res?.code === 2) {
-        setIsModalOpen(true);
-        setUserEmail(username);
-        return;
-      }
-      notification.error({
-        message: "Error Login",
-        description: res?.error,
-      });
-    } else {
-      // redirect to dashboard
-      router.push("/dashboard");
-    }
-  };
+    const onFinish = async (values: any) => {
+        const { username, password } = values;
+        setUserEmail("");
+        //trigger sign-in
+        const res = await authenticate(username, password);
 
-  return (
-    <>
-      <Row justify={"center"} style={{ marginTop: "30px" }}>
-        <Col xs={24} md={16} lg={8}>
-          <fieldset
-            style={{
-              padding: "15px",
-              margin: "5px",
-              border: "1px solid #ccc",
-              borderRadius: "5px",
-            }}
-          >
-            <legend>Đăng Nhập</legend>
-            <Form
-              name="basic"
-              onFinish={onFinish}
-              autoComplete="off"
-              layout="vertical"
-            >
-              <Form.Item
-                label="Email"
-                name="username"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your email!",
-                  },
-                ]}
-              >
-                <Input />
-              </Form.Item>
+        if (res?.error) {
+            //error
+            if (res?.code === 2) {
+                setIsModalOpen(true);
+                setUserEmail(username);
+                return;
+            }
+            notification.error({
+                message: "Error login",
+                description: res?.error
+            })
 
-              <Form.Item
-                label="Password"
-                name="password"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your password!",
-                  },
-                ]}
-              >
-                <Input.Password />
-              </Form.Item>
+        } else {
+            //redirect to /dashboard
+            router.push('/dashboard');
+        }
+    };
 
-              <Form.Item>
-                <Button type="primary" htmlType="submit">
-                  Login
-                </Button>
-              </Form.Item>
-            </Form>
-            <Link href={"/"}>
-              <ArrowLeftOutlined /> Quay lại trang chủ
-            </Link>
-            <Divider />
-            <div style={{ textAlign: "center" }}>
-              Chưa có tài khoản?{" "}
-              <Link href={"/auth/register"}>Đăng ký tại đây</Link>
-            </div>
-          </fieldset>
-        </Col>
-      </Row>
+    return (
+        <>
+            <Row justify={"center"} style={{ marginTop: "30px" }}>
+                <Col xs={24} md={16} lg={8}>
+                    <fieldset style={{
+                        padding: "15px",
+                        margin: "5px",
+                        border: "1px solid #ccc",
+                        borderRadius: "5px"
+                    }}>
+                        <legend>Đăng Nhập</legend>
+                        <Form
+                            name="basic"
+                            onFinish={onFinish}
+                            autoComplete="off"
+                            layout='vertical'
+                        >
+                            <Form.Item
+                                label="Email"
+                                name="username"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your email!',
+                                    },
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
 
-      <ModalReactive
-        isModalOpen={isModalOpen}
-        setIsModalOpen={setIsModalOpen}
-        userEmail={userEmail}
-      />
-    </>
-  );
-};
+                            <Form.Item
+                                label="Password"
+                                name="password"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your password!',
+                                    },
+                                ]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+
+
+
+                            <Form.Item
+                            >
+                                <div style={{
+                                    display: "flex",
+                                    justifyContent: "space-between",
+                                    alignItems: "center"
+                                }}>
+                                    <Button type="primary" htmlType="submit">
+                                        Login
+                                    </Button>
+                                    <Button type='link' onClick={() => setChangePassword(true)}>Quên mật khẩu ?</Button>
+                                </div>
+                            </Form.Item>
+                        </Form>
+                        <Link href={"/"}><ArrowLeftOutlined /> Quay lại trang chủ</Link>
+                        <Divider />
+                        <div style={{ textAlign: "center" }}>
+                            Chưa có tài khoản? <Link href={"/auth/register"}>Đăng ký tại đây</Link>
+                        </div>
+                    </fieldset>
+                </Col>
+            </Row>
+            <ModalReactive
+                isModalOpen={isModalOpen}
+                setIsModalOpen={setIsModalOpen}
+                userEmail={userEmail}
+            />
+            <ModalChangePassword
+                isModalOpen={changePassword}
+                setIsModalOpen={setChangePassword}
+            />
+        </>
+    )
+}
 
 export default Login;
